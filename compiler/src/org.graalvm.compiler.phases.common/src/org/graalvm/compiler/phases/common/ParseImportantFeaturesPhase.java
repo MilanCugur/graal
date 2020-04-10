77ac32ac19f224074c959af28f4678ff1be33d4c
MilanCugur <milancugurovic@gmail.com>
2020-04-10 15:56:01 +0200

    Minor additions.

--- compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/ParseImportantFeaturesPhase.java
+++ compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/ParseImportantFeaturesPhase.java
@@ -243,6 +243,8 @@ public class ParseImportantFeaturesPhase extends BasePhase<CoreProviders> {
             //    System.err.println("CALL_TARGET: [" + ((InvokeNode) node).callTarget().targetName() + "]");
             //}
             System.err.println("INPUTS: "+node.inputs().toString());
+            for(Node ninput : node.inputs())
+                System.err.println("NODE: "+ninput+":"+(ninput instanceof BinaryNode || ninput instanceof LogicNode || ninput instanceof TernaryNode || ninput instanceof UnaryNode));
             if(node instanceof AbstractMergeNode){
                 System.err.println("ABMN: PHIS"+ Arrays.toString(((AbstractMergeNode) node).phis().snapshot().toArray()));
                 System.err.println("ABMN: VALUE PHIS"+Arrays.toString(((AbstractMergeNode) node).valuePhis().snapshot().toArray()));
@@ -471,10 +473,10 @@ public class ParseImportantFeaturesPhase extends BasePhase<CoreProviders> {
         UnmodifiableMapCursor<AbstractBeginNode, List<Block>> __sons = cs.getSons();
         synchronized (writer) {
             long graphId = graph.graphId();
-            int nodeBCI = ((Node) head.getEndNode()).getNodeSourcePosition() == null ? -9999 : ((Node) head.getEndNode()).getNodeSourcePosition().getBCI();  // -9999 represent error BCI code
+            int nodeBCI = ((Node) head.getEndNode()).getNodeSourcePosition() == null ? -9999 : head.getEndNode().getNodeSourcePosition().getBCI();  // -9999 represent error BCI code
             String name = graph.method().getName();
 
-            writer.printf("%d,\"%s\",%s,%d,%d,%d,%s", graphId, name, ((Node) head.getEndNode()).toString(), card, ((Node) head.getEndNode()).getId(), nodeBCI, head);
+            writer.printf("%d,\"%s\",%s,%d,%d,%d,%s", graphId, name, head.getEndNode().toString(), card, head.getEndNode().getId(), nodeBCI, head);
             while (__sons.advance()) {
                 AbstractBeginNode sonHead = __sons.getKey();
                 List<Block> sonPath = __sons.getValue();
@@ -524,7 +526,7 @@ public class ParseImportantFeaturesPhase extends BasePhase<CoreProviders> {
                     writerAttr.printf("; NInvoke: [%d][%d]", getNInvoke(sonPath), getNInvoke(pinnedPath));
                     writerAttr.printf("; NExceptions: [%d][%d]", genNExceptions(sonPath), genNExceptions(pinnedPath));
                     writerAttr.printf("; NAllocations: [%d][%d]", getNAllocations(sonPath), getNAllocations(pinnedPath));
-                    writerAttr.printf("; NSimpleInstr: [0][0]", getNSimpleInstr(sonPath), getNSimpleInstr(pinnedPath));
+                    writerAttr.printf("; NSimpleInstr: [%d][%d]", getNSimpleInstr(sonPath), getNSimpleInstr(pinnedPath));
                 }
             }
             writerAttr.printf("%n");
@@ -659,7 +661,12 @@ public class ParseImportantFeaturesPhase extends BasePhase<CoreProviders> {
         }
         int nnodes = 0;
         for (Block b : path) {
-            nnodes += schedule.nodesFor(b).size();
+            for(Node n : schedule.nodesFor(b)){
+                nnodes += 1;
+                if(n instanceof AbstractMergeNode) {
+                    nnodes += ((AbstractMergeNode)n).phis().snapshot().size();  // Add eventually phi nodes which are not block assigned
+                }
+            }
         }
         return nnodes;
     }
@@ -829,11 +836,42 @@ public class ParseImportantFeaturesPhase extends BasePhase<CoreProviders> {
             return 0;
         }
         int nsimple = 0;
+        EconomicSet<AbstractEndNode> myEnds = EconomicSet.create(Equivalence.DEFAULT);
         for (Block b : path) {
             for (Node n : b.getNodes()) {
-                for(Node ninput : n.inputs())
-                    if(ninput instanceof BinaryNode || ninput instanceof LogicNode || ninput instanceof TernaryNode || ninput instanceof UnaryNode)
-                        nsimple ++;
+                if(n instanceof AbstractEndNode) {
+                    myEnds.add((AbstractEndNode)n);
+                }
+                for(Node ninput : n.inputs()) {
+                    if (ninput instanceof BinaryNode || ninput instanceof LogicNode || ninput instanceof TernaryNode || ninput instanceof UnaryNode) {
+                        nsimple++;
+                    }
+                }
+            }
+        }
+
+        // get appropriate phi floating nodes
+        //writer.printf(", \"[%s][%s]\"", sonPath!=null&&__pathReachable(sonPath)!=null?__pathReachable(sonPath):"null", pinnedPath!=null&&__pathReachable(pinnedPath)!=null?__pathReachable(pinnedPath):"null");
+        System.out.println(path.toString());
+        System.out.println(myEnds);
+        for(AbstractMergeNode mnode : __pathReachable(path)){  // go through my merges
+            EconomicSet<Integer> myindexes = EconomicSet.create(Equivalence.DEFAULT);  // find paths indexes
+            for(int i=0; i<mnode.forwardEnds().size(); i++){
+                AbstractEndNode minput = mnode.forwardEndAt(i);
+                if(myEnds.contains(minput)){
+                    myindexes.add(i);
+                }
+            }
+            System.out.println(myindexes);
+            for(ValuePhiNode mphi : mnode.valuePhis()) { // go through appropriate phi nodes, update simple instructions counter
+                System.out.println(mnode.forwardEnds().toString());
+                System.out.println(mphi.values()+"\n\n");
+                for(Integer i: myindexes) {
+                    Node tmp = mphi.valueAt(i);
+                    if (tmp instanceof BinaryNode || tmp instanceof LogicNode || tmp instanceof TernaryNode || tmp instanceof UnaryNode) {
+                        nsimple++;
+                    }
+                }
             }
         }
         return nsimple;
