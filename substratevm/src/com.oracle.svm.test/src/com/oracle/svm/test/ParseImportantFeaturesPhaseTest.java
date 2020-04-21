09772e07435fa3cf1098454cd7d1d5598c086cce
MilanCugur <milancugurovic@gmail.com>
2020-04-21 18:09:04 +0200

    Branching attributes test added.

--- substratevm/src/com.oracle.svm.test/src/com/oracle/svm/test/ParseImportantFeaturesPhaseTest.java
+++ substratevm/src/com.oracle.svm.test/src/com/oracle/svm/test/ParseImportantFeaturesPhaseTest.java
@@ -173,10 +173,154 @@ public class ParseImportantFeaturesPhaseTest extends GraalCompilerTest {
         System.out.printf("%3s %30s %s\n", "blk", "successors", "blk_nodes:");
         for (Block b : cfg.getBlocks()) {
             System.out.printf("%3s %30s ", b.toString(), Arrays.toString(b.getSuccessors()));
-            System.out.print(b.getNodes());
+            //System.out.print(b.getNodes());
             System.out.println(r.nodesFor(b));
         }
         System.out.println("------------------------------------------------------------------------------------------------------------");
+    }
+
+    private void testAttributesCodeSnippet(String snippet, String[] attributesTruth, String groundTruth) {
+        StructuredGraph graph = parseEager(snippet, StructuredGraph.AllowAssumptions.NO);
+        ParseImportantFeaturesPhase p = new ParseImportantFeaturesPhase(snippet);
+        p.apply(graph, null);
+
+        ControlFlowGraph cfg = ControlFlowGraph.compute(graph, true, true, true, true);
+        try {
+            SchedulePhase.run(graph, SchedulePhase.SchedulingStrategy.LATEST, cfg);  // Do [LASTEST] scheduling cause of floating point nodes
+        } catch (Throwable t) {
+            throw graph.getDebug().handle(t);
+        }
+        StructuredGraph.ScheduleResult r = graph.getLastSchedule();
+
+        // Parse ground truth
+        HashMap<String, HashMap<String, String[]>> gt = new HashMap<>();
+        String[] lines = groundTruth.split("\\r?\\n");
+        for (String line : lines) {
+            String[] parts = line.split("-");
+            String NodeDescription = parts[0].strip();
+            String head = parts[1].strip();
+            HashMap<String, String[]> sons = new HashMap<>();
+            for (int index = 2; index < parts.length; index++) {
+                // src
+                String[] src = parts[index].split(";");
+                assert src.length == 2 : "Information not valid: " + parts[index];
+
+                // branch + tail info
+                String son = src[0];
+                String[] sonData = son.split("\\]\\[");
+                assert sonData.length == 2 : "ParseImportantFeaturesPhaseTest Error: Ground Truth data invalid.";
+                String branch = __sortPath(sonData[0].replaceAll("\\[", "").replaceAll("^\"|\"$", ""));
+                String tail = __sortPath(sonData[1].replaceAll("\\]", "").replaceAll("^\"|\"$", ""));
+
+                // attributes info
+                // ex. "17|If-B4-[B5][null];[0][0]:[0][0]"
+                String attr = src[1];
+                String[] attrData = attr.split(":");
+                assert attrData.length == attributesTruth.length : "ParseImportantFeaturesPhaseTest Error: ground truth not valid, attributes: " + attr + " inside: " + branch + "--" + tail;
+                sons.put(branch + "--" + tail, attrData);
+            }
+            gt.put(NodeDescription + "--" + head, sons);
+        }
+
+        // Parse .csv
+        int nerror = 0;
+        try {
+            File attributes = __getAttributes(snippet);
+            //System.out.println("Working with attributes: " + attributes);
+            assert attributes != null : "ParseImportantFeaturesPhaseTest: cannot find .csv attributes";
+            BufferedReader csv = new BufferedReader(new FileReader(attributes));
+            String line;
+            while ((line = csv.readLine()) != null) {
+                if (line.equals("Graph Id, Source Function, Node Description, head, CD Depth, N. CS Father Blocks, N. CS Father Fixed Nodes, N. CS Father Floating Nodes"))  // skip header line
+                    continue;
+                String[] data = line.split(",(?!\\s)");
+                String SourceFunction = data[1].replaceAll("\"", "");
+                assert SourceFunction.equals(snippet) : "ParseImportantFeaturesPhaseTest Error: Source function does not match.";
+                String NodeDescription = data[2];
+                String head = data[3];
+                assert gt.containsKey(NodeDescription + "--" + head) : "ParseImportantFeaturesPhaseError: wrong Control Split in .csv file.";
+                HashMap<String, String[]> sons = gt.get(NodeDescription + "--" + head);
+                HashMap<String, String> csData = new HashMap<>();
+                csData.put("CS Depth", data[4]);
+                csData.put("N. CS Father Blocks", data[5]);
+                csData.put("N. CS Father Fixed Nodes", data[6]);
+                csData.put("N. CS Father Floating Nodes", data[7]);
+
+                for (int index = 8; index < data.length; index++) {
+                    // src
+                    String[] src = data[index].split(";");
+
+                    // son gt info
+                    HashMap<String, String> sonData = new HashMap<String, String>(csData);
+                    for (int i = 1; i < src.length; i++) {
+                        // ex.: "N. Array Store: [0][0]"
+                        String attr = src[i];
+                        sonData.put(attr.split(":")[0].strip(), attr.split(":")[1].strip());
+                    }
+
+                    // head + tail info
+                    String[] branchData = src[0].split(Pattern.quote("]["));
+                    assert branchData.length == 2 : "ParseImportantFeaturesPhaseTest Error: Invalid branch data.";
+                    String branch = __sortPath(branchData[0].replaceAll("\\[", "").replaceAll("^\"|\"$", ""));
+                    String tail = __sortPath(branchData[1].replaceAll("\\]", "").replaceAll("^\"|\"$", ""));
+
+                    if (!sons.containsKey(branch + "--" + tail)){
+                        System.out.println("ParseImportantFeaturesPhaseTest error on function: " + snippet + " Error son not found: " + branch + "--" + tail);
+                        System.out.println("Ground Truth: " + sons.keySet());
+                        nerror += 1;
+                    } else if(!__compare(attributesTruth, sons.get(branch + "--" + tail), sonData)){
+                        System.out.println("ParseImportantFeaturesPhaseTest error on function: " + snippet + " Content aren't the same on son: " + branch + "--" + tail);
+                        System.out.println("Ground Truth: " + sons.keySet());
+                        nerror += 1;
+                    }else{
+                        sons.remove(branch + "--" + tail);
+                    }
+                }
+                if (sons.size() > 0) {
+                    System.out.println("ParseImportantFeaturesPhaseTest error on function: " + snippet + " son[s] not found in parsed data: ");
+                    sons.keySet().stream().forEach(son -> System.out.println(son));
+                    nerror += 1;
+                }
+            }
+            csv.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(".csv file with parsed blocks are not found. ParseImportantFeaturesPhase - output file error.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println(".csv file with parsed blocks are not found. ParseImportantFeaturesPhase - line read error.");
+            e.printStackTrace();
+        }
+
+        assertTrue("Test " + snippet + " failed.", nerror == 0);
+    }
+
+    public boolean __compare(String[] attributesTruth, String[] gtData, HashMap<String, String> parsedData) {
+        // [2][0] vs [2][0] or [1] vs 1
+        HashSet<String> unique = new HashSet<>();
+        unique.addAll(Arrays.asList(new String[]{"CS Depth", "N. CS Father Blocks", "N. CS Father Fixed Nodes", "N. CS Father Floating Nodes"}));
+
+        for (int i = 0; i < attributesTruth.length; i++) {
+            String attribute = attributesTruth[i];
+            String gt = gtData[i];
+            String parsed = parsedData.get(attribute);
+            assert parsed != null : "ParseImportantFeaturesPhaseError: cannot find attribute: " + attribute;
+            if (unique.contains(attribute)) {
+                if (Integer.parseInt(parsed) != Integer.parseInt(gt.replace("[", "").replace("]", ""))) {
+                    System.out.println("__compare Error: TRUE" + gt + " PARSED: " + parsed);
+                    return false;
+                }
+            } else {
+                int gtBranch = Integer.parseInt(gt.split("\\]\\[")[0].replace("[", "").replace("]", ""));
+                int gtTail = Integer.parseInt(gt.split("\\]\\[")[1].replace("[", "").replace("]", ""));
+                int parsedBranch = Integer.parseInt(parsed.split("\\]\\[")[0].replace("[", "").replace("]", ""));
+                int parsedTail = Integer.parseInt(parsed.split("\\]\\[")[1].replace("[", "").replace("]", ""));
+                if (gtBranch != parsedBranch || gtTail != parsedTail) {
+                    System.out.println("__compare Error: TRUE" + gt + " PARSED: " + parsed);
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     /* TESTS */
@@ -279,9 +423,18 @@ public class ParseImportantFeaturesPhaseTest extends GraalCompilerTest {
         testCodeSnippet("example_ftest49", groundTruth);
     }
 
+    // NodeDescription-Head-Branch1-Branch2-Branch3
+    // BranchI: [son][tail];[son attr 1][tail attr 1]:[son attr 2][tail attr 2]:[cs global attr]:etc.
+    // attr correspond to attributes respectively
     @Test
-    public void test_ControlSplit(){
-        __printCodeSnippet("example_ControlSplits");
+    public void test_ControlSplit() {
+        String groundTruth = "9|If-B0-[B1,B2,B3,B4,B5,B6][null];[2][0]:[2][0]:[0]:[0]:[0]:[0]-[B7,B8,B9,B11,B12,B13][null];[2][0]:[2][0]:[0]:[0]:[0]:[0]\n" +
+                "10|IntegerSwitch-B1-[B2][null];[0][0]:[0][0]:[1]:[13]:[26]:[16]-[B3][null];[0][0]:[0][0]:[1]:[13]:[26]:[16]-[B4,B5,B6][null];[2][0]:[1][0]:[1]:[13]:[26]:[16]\n" +
+                "17|If-B4-[B5][null];[0][0]:[0][0]:[2]:[6]:[12]:[5]-[B6][null];[0][0]:[0][0]:[2]:[6]:[12]:[5]\n" +
+                "42|If-B8-[x(39|LoopExit)][null];[0][0]:[0][0]:[1]:[13]:[26]:[16]-[B11,B12,B13][null];[2][0]:[1][0]:[1]:[13]:[26]:[16]\n" +
+                "46|If-B11-[B12][null];[0][0]:[0][0]:[2]:[5]:[10]:[5]-[B13][null];[0][0]:[0][0]:[2]:[5]:[10]:[5]\n";
+        String[] attributes = {"Max CS Depth", "N. Control Splits", "CS Depth", "N. CS Father Blocks", "N. CS Father Fixed Nodes", "N. CS Father Floating Nodes"};
+        testAttributesCodeSnippet("example_ControlSplits", attributes, groundTruth);
     }
 
     /* SOURCE CODES */
@@ -528,11 +681,11 @@ public class ParseImportantFeaturesPhaseTest extends GraalCompilerTest {
         return;
     }
 
-    /* ATTRIBUTES TESTS SOURCE CODES */
-    private static int example_ControlSplits(int a, int b, int c){
+    /* SOURCE CODES FOR ATTRIBUTES TESTS */
+    private static int example_ControlSplits(int a, int b, int c) {
         int tmp = 11;
-        if(a>b){
-            switch(c){
+        if (a > b) {
+            switch (c) {
                 case 1:
                     tmp += b;
                     break;
@@ -540,15 +693,15 @@ public class ParseImportantFeaturesPhaseTest extends GraalCompilerTest {
                     tmp *= b;
                     break;
                 default:
-                    if(c>b)
+                    if (c > b)
                         tmp = 9;
                     else
                         tmp = 7;
             }
-        }else{
+        } else {
             int i = 0;
-            while(i<a){
-                if(tmp>b)
+            while (i < a) {
+                if (tmp > b)
                     tmp -= c;
                 else
                     tmp += a;
